b35fabceb90abdf0badf471b5830d1ff0adf7fca
Namik Heydarov <heydarovnamiq@gmail.com>
2022-02-10 01:24:36 +0400

    updated

--- FirstFiorellaMVC/Controllers/HomeController.cs
+++ FirstFiorellaMVC/Controllers/HomeController.cs
@@ -1,10 +1,14 @@
 ﻿using FirstFiorellaMVC.DataAccessLayer;
 using FirstFiorellaMVC.Models;
 using FirstFiorellaMVC.ViewModels;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
+using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Product = FirstFiorellaMVC.Models.Product;
 
 namespace FirstFiorellaMVC.Controllers
 {
@@ -54,6 +58,88 @@ namespace FirstFiorellaMVC.Controllers
                 .ToListAsync();
 
             return PartialView("_SearchProductPartial", products);
+        }
+
+        public async Task<IActionResult> Basket()
+        {
+            var basket = Request.Cookies["Basket"];
+
+            if (string.IsNullOrEmpty(basket))
+            {
+                return Content("Empthy");
+            }
+
+            var basketViewModels = JsonConvert.DeserializeObject<List<BasketViewModel>>(basket);
+            var newBaskets = new List<BasketViewModel>();
+            foreach (var item in basketViewModels)
+            {
+                var product = await _appDbContext.Products.FindAsync(item.Id);
+                if (product == null)
+                    continue;
+
+                newBaskets.Add(new BasketViewModel
+                {
+                    Id = product.Id,
+                    Name = product.Name,
+                    Price = product.Price,
+                    Dimension = product.Dimension,
+                    SKUCode = product.SKUCode,
+                    Weight = product.Weight,
+                    Images = product.Images,
+                    Count = item.Count,
+                });
+            }
+
+            basket = JsonConvert.SerializeObject(newBaskets);
+
+            Response.Cookies.Append("Basket", basket, new CookieOptions { Expires = System.DateTimeOffset.Now.AddDays(1) });
+            return View(newBaskets);
+        }
+
+        public async Task<IActionResult> AddBasket(int? id)
+        {
+            if(id == null)
+                return BadRequest();
+
+            var product = await _appDbContext.Products.FindAsync(id);
+            if (product == null)
+                return NotFound();
+
+            List<BasketViewModel> basketViewModels;
+            var CookieBasket = Request.Cookies["Basket"];
+            if(string.IsNullOrEmpty(CookieBasket))
+            {
+                basketViewModels = new List<BasketViewModel>();
+            }
+            else
+            {
+                basketViewModels = JsonConvert.DeserializeObject<List<BasketViewModel>>(CookieBasket);
+            }
+
+            var existBasketViewModel = basketViewModels.FirstOrDefault(x => x.Id == id);
+            if(existBasketViewModel == null)
+            {
+                basketViewModels.Add(new BasketViewModel()
+                {
+                    Id = product.Id,
+                    Name = product.Name,
+                    Price = product.Price,
+                    Dimension = product.Dimension,
+                    SKUCode = product.SKUCode,
+                    Weight = product.Weight,
+                    Images = product.Images,
+                });
+            }
+            else
+            {
+                existBasketViewModel.Count++;
+            }
+
+            var Basket = JsonConvert.SerializeObject(basketViewModels);
+
+            Response.Cookies.Append("Basket", Basket, new CookieOptions { Expires = System.DateTimeOffset.Now.AddDays(1)});
+
+            return RedirectToAction(nameof(Index));
         }
     }
 }
---
+++ FirstFiorellaMVC/ViewModels/BasketViewModel.cs
@@ -0,0 +1,25 @@
+﻿using FirstFiorellaMVC.Models;
+using System.Collections.Generic;
+
+namespace FirstFiorellaMVC.ViewModels
+{
+    public class BasketViewModel
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public double Price { get; set; }
+
+        public string Dimension { get; set; }
+
+        public string Weight { get; set; }
+
+        public string SKUCode { get; set; }
+
+        public int Count { get; set; } = 1;
+
+        public List<ProductImage> Images { get; set; }
+
+    }
+}
